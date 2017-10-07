214ac6dfe237edc90e33f63b01a16fe06376e697
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-10-07 14:19:49 +1300

    test

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "208e45a8-30de-43c0-b57a-f1e876d58e5d",
+    appPassword: "WoO1QjAsWyn73dbcHa04qWn"
 });
 
 // Listen for messages from users 
