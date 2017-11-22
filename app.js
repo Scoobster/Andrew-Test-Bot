09019e7fecb1a510dbee7d0e7057109935f733bd
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-11-23 11:04:22 +1300

    added restify stuff + debugging

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:') 
 var bot = new builder.UniversalBot(connector, function (session) { 
     session.send("You said: " + session.message.text); 
-}
+});
