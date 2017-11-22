8bb660deaaf109b82aea0eefa264b4c96313114e
Andrew Lyall <Scoobster@users.noreply.github.com>
2017-11-23 10:39:47 +1300

    Removed bad language

--- app.js
+++ app.js
@@ -18,21 +18,6 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You fucking said: " + session.message.text);
-    //console.log(session);
-    /*var fd = new FormData(session.message.attachments);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "australiaeast.api.cognitive.microsoft.com/vision/v1.0/analyze", true);
-    xhr.setRequestHeader("Ocp-Apim-Subscription-Key", "a4caf9d-d420-4582-973d-640e644ead69");
-    xhr.onload = function (data) {
-        console.log("SUCCESS");
-        console.log(data);
-        session.send(data.description.captions[0].text);
-    }
-    xhr.onerror = function (data) {
-        session.send("I FAILED YOU!");
-    }
-    xhr.send(fd);*/
+    session.send("You said: " + session.message.text);
 
 });
