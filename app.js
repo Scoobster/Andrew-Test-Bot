949eff78234688efcf687e3d42e28ea72c9858ad
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-11-23 13:36:51 +1300

    still trying to fix it

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
  
 // Create chat connector for communicating with the Bot Framework Service 
 var connector = new builder.ChatConnector({ 
-    appId: 9019cbc8-8055-4180-8f2e-53051ea28deb, 
-    appPassword: ijrQ7275!vzheCZPZMS1%=$ 
+    appId: "9019cbc8-8055-4180-8f2e-53051ea28deb", 
+    appPassword: "ijrQ7275!vzheCZPZMS1%=$"
 }); 
  
 // Listen for messages from users   
